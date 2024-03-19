4f5d175f8815c7b397634ce9c97a216d23105023
Amit Kandar <kandaramit2001@gmail.com>
2024-03-19 22:16:59 +0530

    Added few routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
+import Courses from '../pages/Courses';
+import Features from '../pages/Features';
 
 function AppRouter() {
     return (
@@ -10,6 +12,8 @@ function AppRouter() {
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
+            <Route path="/courses" element={<Courses />} />
+            <Route path="/features" element={<Features />} />
         </Routes>
     );
 }
