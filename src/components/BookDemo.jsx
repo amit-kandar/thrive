40de0fa30661b56fc7f766706bd6eb8e94e6bd77
Amit Kandar <kandaramit2001@gmail.com>
2024-03-20 10:41:36 +0530

    Change background color

--- src/components/BookDemo.jsx
+++ src/components/BookDemo.jsx
@@ -4,7 +4,7 @@ import { BOOK_DEMO_BG, BOOK_DEMO_BUTTON_TEXT, BOOK_DEMO_DESCRIPTION, BOOK_DEMO_I
 function App() {
     return (
         <section className="relative w-full h-[700px] md:h-96 flex justify-between" style={{ backgroundImage: `url(${SECTION_BG_IMG})`, backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
-            <div className="absolute w-full h-1/2 bg-yellow-500"></div>
+            <div className="absolute w-full h-1/2 bg-yellow-500/10"></div>
             <div className='w-full h-full flex justify-center' style={{ backgroundColor: 'rgba(247, 252, 197, 0.3)' }}></div>
             <div className="absolute w-full max-w-96 md:max-w-7xl md:w-11/12 h-full md:h-72 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 rounded-lg" style={{ backgroundImage: `url(${BOOK_DEMO_BG})`, backgroundPosition: "center", backgroundSize: "cover", backgroundRepeat: "no-repeat" }}>
                 <div className='w-full h-full flex flex-col items-center md:flex-row md:gap-5 rounded-lg p-4' style={{ backgroundColor: "rgba(0, 121, 226, 0.8)" }}>
