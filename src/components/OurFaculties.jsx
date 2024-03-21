b6489531e79074ba758c50266837ed58d260b424
Amit Kandar <kandaramit2001@gmail.com>
2024-03-21 15:05:54 +0530

    Fixed a bug

--- src/components/OurFaculties.jsx
+++ src/components/OurFaculties.jsx
@@ -8,7 +8,7 @@ function OurFaculties() {
                 <h1 className='text-2xl text-center md:text-4xl font-medium text-black'>Faculty Excellence</h1>
                 <h3 className='text-sm text-center md:text-base text-yellow-400/80'>Meet Our Distinguished Educators and Researchers</h3>
             </div>
-            <div className='w-full mt-5'>
+            <div className='w-full mt-10 lg:mt-20'>
                 <InfiniteMovingCards />
             </div>
         </section>
