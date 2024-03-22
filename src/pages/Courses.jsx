0e1456114620789babdc1482f8b8d5665233a678
Amit Kandar <kandaramit2001@gmail.com>
2024-03-22 23:45:14 +0530

    Fixed a bug

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { CONTACT_TITLE, CONTACT_SUB_HEADING } from '../constants'
+import { COURSE_TITLE, COURSE_SUB_HEADING } from '../constants'
 import { COURSES_DETAILS } from '../utils/courseDetails'
 import CourseCard from '../components/CourseCard'
 
@@ -7,8 +7,8 @@ function Courses() {
     return (
         <section className='w-full bg-gray-200 px-5 py-20'>
             <div className='flex flex-col items-start px-3'>
-                <h1 className='text-3xl lg:text-5xl font-medium text-gray-700'>{CONTACT_TITLE}</h1>
-                <span className='text-base text-gray-500 lg:text-lg'>{CONTACT_SUB_HEADING}</span>
+                <h1 className='text-3xl lg:text-5xl font-medium text-gray-700'>{COURSE_TITLE}</h1>
+                <span className='text-base text-gray-500 lg:text-lg'>{COURSE_SUB_HEADING}</span>
                 <div className='w-full lg:flex justify-center mt-10'>
                     <div className='grid place-items-center gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
                         {
